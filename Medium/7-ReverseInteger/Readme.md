1a07ada8dfbccc9ac2a72168b5954a587f12fe40
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-03-21 15:18:01 +0300

    Added Reverse Integer

---
+++ Medium/7-ReverseInteger/Readme.md
@@ -0,0 +1,6 @@
+# Reverse Integer
+
+#### [Question Link](https://leetcode.com/problems/reverse-integer/)
+
+#### Result
+![result](Result.png)
---
+++ Medium/7-ReverseInteger/ReverseInteger.java
@@ -0,0 +1,66 @@
+public class ReverseInteger {
+
+    /*
+
+    Given a signed 32-bit integer x, return x with its digits reversed.
+    If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+    Example 1:
+
+    Input: x = 123
+    Output: 321
+
+    Example 2:
+
+    Input: x = -123
+    Output: -321
+
+    Example 3:
+
+    Input: x = 120
+    Output: 21
+
+    Example 4:
+
+    Input: x = 0
+    Output: 0
+
+     */
+
+    // Link : https://leetcode.com/problems/reverse-integer/
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(120));
+        System.out.println(reverse(0));
+
+    }
+
+    public static int reverse(int x) {
+
+        int reverse = 0;
+
+        while(x != 0){
+
+            int digit = x % 10;
+
+            x /= 10;
+
+            if(reverse > Integer.MAX_VALUE/10 || (reverse == Integer.MAX_VALUE/10 && digit > 7))
+                return 0;
+            if(reverse < Integer.MIN_VALUE/10 || (reverse == Integer.MIN_VALUE/10 && digit < -8))
+                return 0;
+
+            reverse = reverse * 10 + digit;
+
+        }
+
+        return reverse;
+
+    }
+
+}
