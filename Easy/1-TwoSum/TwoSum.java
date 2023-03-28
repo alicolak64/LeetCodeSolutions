945649636635f5f426299f56baf35d2d7fba264b
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-03-28 16:28:30 +0300

    Added Two Sum

--- Easy/1-TwoSum/TwoSum.java
+++ Easy/1-TwoSum/TwoSum.java
@@ -1,83 +1,60 @@
-import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
 
-    // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-    // Example:
-    // Given nums = [2, 7, 11, 15], target = 9,
-    // Because nums[0] + nums[1] = 2 + 7 = 9,
-    // return [0, 1].
+    /*
 
-    // Link : https://leetcode.com/problems/two-sum/
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+    Example 1:
+
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    Example 2:
+
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
+    Example 3:
+
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+
+
+    Follow up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+     */
 
     public static void main(String[] args) {
 
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
-        int[] result = twoSum(nums, target);
-        System.out.println(Arrays.toString(result));
+        for (int i : twoSum(nums, target))
+            System.out.print(i + " ");
 
     }
 
     public static int[] twoSum(int[] nums, int target) {
 
-        int[] numbers = Arrays.copyOf(nums, nums.length);
+        HashMap <Integer, Integer> map = new HashMap<>();
 
-        Arrays.sort(numbers);
+        for (int i = 0; i < nums.length; i++) {
 
-        int[] indexArray = {-1, -1};
+            if (map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]), i};
 
-        out:
-        for (int i = 0; i < numbers.length; i++) {
-            in:
-            for (int j = i + 1; j < numbers.length; j++) {
-
-                if (numbers[i] + numbers[j] > target)
-                    break in;
-                else if (numbers[i] + numbers[j] == target) {
-                    indexArray[0] = numbers[i];
-                    indexArray[1] = numbers[j];
-                    break out;
-                }
-
-            }
+            map.put(nums[i], i);
 
         }
 
-
-        if (indexArray[0] == indexArray[1]) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == indexArray[0]) {
-                    indexArray[0] = i;
-                    break;
-                }
-            }
-            for (int i = nums.length - 1; i >= 0; i--) {
-                if (nums[i] == indexArray[1]) {
-                    indexArray[1] = i;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == indexArray[0]) {
-                    indexArray[0] = i;
-                    break;
-                }
-            }
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == indexArray[1]) {
-                    indexArray[1] = i;
-                    break;
-                }
-            }
-        }
-
-        Arrays.sort(indexArray);
-
-        return indexArray;
-
+        return new int[] {-1, -1};
 
     }
 
