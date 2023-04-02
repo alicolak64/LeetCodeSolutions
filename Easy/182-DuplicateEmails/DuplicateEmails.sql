7465247cbd43c856071665623067ea2839e4304e
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2023-04-03 02:53:34 +0300

    Added Duplicate Emails

--- Easy/182-DuplicateEmails/DuplicateEmails.sql
+++ Easy/182-DuplicateEmails/DuplicateEmails.sql
@@ -1,3 +1,50 @@
+/*
+
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+*/
+
+-- Link : https://leetcode.com/problems/duplicate-emails/
+
+-- Write your MySQL query statement below
+
 SELECT email
 FROM Person
 GROUP BY email
