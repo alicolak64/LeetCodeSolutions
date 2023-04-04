d024273b15e9cbcd4a9f9a7f7cb527581cdc3474
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2023-04-04 13:02:32 +0300

    Added Rising Temperature

---
+++ Easy/197-RisingTemperature/Readme.md
@@ -0,0 +1,6 @@
+# Rising Temperature
+
+#### [Question Link](https://leetcode.com/problems/rising-temperature/)
+
+#### Result
+![result](Result.png)
---
+++ Easy/197-RisingTemperature/RisingTemperature.sql
@@ -0,0 +1,55 @@
+/*
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+*/
+
+-- Link : https://leetcode.com/problems/rising-temperature/
+
+-- Write your MySQL query statement below
+
+SELECT weather1.id FROM Weather weather1, Weather weather2
+WHERE datediff(weather1.recordDate, weather2.recordDate)= 1
+AND weather1.temperature > weather2.temperature;
